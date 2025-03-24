d497d1fd22fe080627758a79c02662c7834e69d2
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-03-24 12:08:47 -0400

    Remove add vault button when field is full and change profile button to hamburger.

--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -34,7 +34,7 @@ export default function NavBar() {
             <Menu as="div" className="relative">
               <div>
                 <MenuButton
-                  className="flex rounded-full text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-none cursor-pointer"
+                  className="flex text-sm focus:outline-none cursor-pointer"
                   onClick={() => setDrawerOpen(true)}
                 >
                   <span className="sr-only">Open user menu</span>
@@ -49,7 +49,7 @@ export default function NavBar() {
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                      d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                     />
                   </svg>
                 </MenuButton>
