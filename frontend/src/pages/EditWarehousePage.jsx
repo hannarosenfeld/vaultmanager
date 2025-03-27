c5d098a02e1ae0d622ddab3ec6b1979d9c92a538
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-03-27 12:38:55 -0400

    Style button.

--- frontend/src/pages/EditWarehousePage.jsx
+++ frontend/src/pages/EditWarehousePage.jsx
@@ -73,8 +73,8 @@ export default function EditWarehousePage() {
         </div>
         <button
           onClick={handleSubmit}
-          className="ml-1 px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-        >
+          className="ml-2 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          >
           Submit
         </button>
       </div>
