db9e214df757373fe0b200f6b8132cccda16fac3
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-04-15 10:02:54 -0400

    Fix foreign key schema for pallets

--- app/models/pallet.py
+++ app/models/pallet.py
@@ -1,4 +1,4 @@
-from app.models import db, environment, SCHEMA
+from app.models.db import db, environment, SCHEMA, add_prefix_for_prod
 
 class Pallet(db.Model):
     __tablename__ = 'pallets'
@@ -9,7 +9,11 @@ class Pallet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     weight = db.Column(db.Float, nullable=False)
-    shelf_id = db.Column(db.Integer, db.ForeignKey('shelves.id'), nullable=False)
+    shelf_id = db.Column(
+        db.Integer,
+        db.ForeignKey(add_prefix_for_prod('shelves.id')),  # Use add_prefix_for_prod
+        nullable=False
+    )
     file_path = db.Column(db.String(255), nullable=True)
     note = db.Column(db.Text, nullable=True)
 
