ba0b6a8bc3d4399e4394c9bed3f86cfc5f4c12d6
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-06-04 12:01:34 -0400

    Merge heads.

---
+++ migrations/versions/50e96011944a_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 50e96011944a
+Revises: d7d4bbfb5c65, 8a43d4e1dcb1
+Create Date: 2025-06-04 12:01:22.084821
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '50e96011944a'
+down_revision = ('d7d4bbfb5c65', '8a43d4e1dcb1')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
